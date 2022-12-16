223e43a2b5d81dfa1af43e7b0b839f81b0eec51f
Tapeline <yt.redstone.mail@gmail.com>
2022-12-16 09:54:52 +0500

    snapshot step 5 done 16-12-22 05:00 UTC

--- src/main/java/me/tapeline/quailj/utils/ErrorFormatter.java
+++ src/main/java/me/tapeline/quailj/utils/ErrorFormatter.java
@@ -9,9 +9,10 @@ public class ErrorFormatter {
     public static String formatError(String code, int line, int character,
                                      int length, String errorType, String message) {
         String[] lines = code.split("\n");
+        int index = line != 0? line - 1 : 0;
         StringBuilder error = new StringBuilder();
         error.append("Error on line ").append(line).append(" character ").append(character).append(":\n");
-        for (int i = (line >= 2? line - 2 : line); i <= line; i++) {
+        for (int i = (index >= 2? index - 2 : index); i < line; i++) {
             StringBuilder tabs = new StringBuilder("        ");
             String lineNumStr = Integer.toString(i + 1);
             tabs.replace(tabs.length() - lineNumStr.length(), tabs.length(), lineNumStr);
@@ -24,7 +25,11 @@ public class ErrorFormatter {
         for (int i = 0; i < length; i++)
             underline.append("^");
         error.append(underline).append("\n");
-        for (int i = 0; i < Math.max(underline.length(), message.length()); i++)
+        int maxLength = 0;
+        for (int i = 0; i < lines.length; i++)
+            if (lines[i].length() > maxLength)
+                maxLength = lines[i].length();
+        for (int i = 0; i < Math.max(underline.length(), maxLength); i++)
             error.append("=");
         error.append("\n").append(errorType).append(":\n").append(message);
         return error.toString();
